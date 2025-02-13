299807d8c2db35226e8078d9d5a718cb94c6c18b
MarceloDev94 <marcelo.ssz@hotmail.com>
2025-02-13 02:42:14 -0300

    commit final

--- desafio/app.js
+++ desafio/app.js
@@ -17,29 +17,38 @@ new Vue({
             this.running = true
             this.playerLife = 100
             this.monsterLife = 100
+            this.logs = []
         },
         attack(especial) {
-            this.hurt('mosterLife', 5, 10, especial)
-            this.hurt('playerLife', 7, 12, false)
+            this.hurt('mosterLife', 5, 10, especial, 'Jogador', "Monstro", 'player')
+            if(this.monsterLife > 0) {
+                this.hurt('playerLife', 7, 12, false, "Monstro", "Jogador", 'monster')
+
+            }
 
         },
         healAndHurt() {
             this.heal(10, 15)
-            this.hurt('playerLife', 7, 12, false)
+            this.hurt('playerLife', 7, 12, false, 'Monstro', "Jogador", 'monster')
         },
         heal(min, max) {
             const heal = this.getRandom(min, max)
             this.playerLife = Math.min(this.playerLife + heal, 100)
+            this.registerLog(`Jogador curou-se em ${heal}.`, 'player')
         },
-        hurt(prop, min, max, especial) {
+        hurt(prop, min, max, especial, source, target, cls) {
             const plus = especial ? 5 : 0
             const hurt = this.getRandom(min + plus, max + plus)
             this[prop] = Math.max(this[prop] - hurt, 0)
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls)
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
         },
+        registerLog( text, cls) {
+            this.logs.unshift({text, cls})
+        }
 
     },
     watch: {
